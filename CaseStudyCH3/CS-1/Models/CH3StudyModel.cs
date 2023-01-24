11599f7ab0cb09c25e0f16b566ebc6ce08bb0f41
Sam-Parrish <soupcan120@gmail.com>
2023-01-24 16:11:58 -0600

    Pushy Thingy

--- CaseStudyCH3/CS-1/Models/CH3StudyModel.cs
+++ CaseStudyCH3/CS-1/Models/CH3StudyModel.cs
@@ -2,8 +2,6 @@ namespace CS_1.Models
 {
     public class CH3StudyModel
     {
-        public string? RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+	}
 }
