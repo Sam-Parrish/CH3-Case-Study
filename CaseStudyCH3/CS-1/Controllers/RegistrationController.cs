312df0b4989408abdf43d99fa2f8fcbe29a2b1a6
Sam-Parrish <soupcan120@gmail.com>
2023-04-11 15:59:46 -0500

    Registration Push

---
+++ CaseStudyCH3/CS-1/Controllers/RegistrationController.cs
@@ -0,0 +1,46 @@
+﻿using CS_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS_1.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private const string TECH_KEY = "techID";
+
+        private SportsProContext Context { get; set; }
+
+        public RegistrationController(SportsProContext ctx) => Context = ctx;
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.SelectedCategoryName = "Register";
+            ViewBag.Customers = Context.Customers.OrderBy(c => c.FirstName).ToList();
+
+            var customer = new Customer();
+
+            int? custID = HttpContext.Session.GetInt32(TECH_KEY);
+            if (custID.HasValue)
+            {
+                customer = Context.Customers.Find(custID);
+            }
+
+            return View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult List(Customer customer)
+        {
+            if (customer.CustomerId == 0)
+            {
+                TempData["message"] = "Please select a customer";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                HttpContext.Session.SetInt32(TECH_KEY, customer.CustomerId);
+                return RedirectToAction("List", new { id = customer.CustomerId });
+            }
+        }
+    }
+}
---
+++ CaseStudyCH3/CS-1/Models/RegistrationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace CS_1.Models
+{
+    public class RegistrationViewModel
+    {
+        public Technician Technician { get; set; } = null!;
+        public Incident Incident { get; set; } = null!;
+        public Customer Customer { get; set; } = null!;
+        public Product Product { get; set; } = null!;
+
+        public IEnumerable<Product> Products { get; set; } = null!;
+    }
+}
